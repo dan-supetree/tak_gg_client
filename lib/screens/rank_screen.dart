fafe7b0ae1345d0ac858a65cbc7978312154a78e
dan-supetree <danyj@supertree.co>
2023-07-31 16:09:57 +0900

    feat: create screens && update default screen

---
+++ lib/screens/rank_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RankScreen extends StatefulWidget {
+  const RankScreen({super.key});
+
+  @override
+  State<RankScreen> createState() => _RankScreenState();
+}
+
+class _RankScreenState extends State<RankScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
---
+++ lib/screens/result_submit_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ResultSubmitScreen extends StatefulWidget {
+  const ResultSubmitScreen({super.key});
+
+  @override
+  State<ResultSubmitScreen> createState() => _ResultSubmitScreenState();
+}
+
+class _ResultSubmitScreenState extends State<ResultSubmitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
