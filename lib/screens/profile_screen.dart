402ec0c822a15cb89be2c25b17b9353f3e111c26
dan-supetree <danyj@supertree.co>
2023-08-11 16:23:08 +0900

    feat: update Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tak_gg/states/UserController.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,14 +12,106 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
+    final UserController userController = Get.put(UserController());
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('My Profile'),
+          backgroundColor: Colors.blue,
+          iconTheme: const IconThemeData(color: Colors.white),
         ),
         body: Padding(
-            padding: const EdgeInsets.all(32.0),
+            padding: const EdgeInsets.symmetric(horizontal: 32),
             child: Column(
-              children: const [Text('Profile')],
+              children: [
+                Transform.scale(
+                  scale: 2.5,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(100),
+                        bottomRight: Radius.circular(100)),
+                    child: Container(
+                      width: 200,
+                      height: 90,
+                      color: Colors.blue,
+                      child: Align(
+                          alignment: Alignment.center,
+                          child: Text(userController.displayName,
+                              style: const TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white))),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(userController.profileImage)),
+                const SizedBox(height: 32),
+                Column(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                        width: 1.0,
+                        color: Colors.black12,
+                      ))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Racket',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.w600)),
+                            Text(userController.racket ?? 'None')
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                        width: 1.0,
+                        color: Colors.black12,
+                      ))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Rubber',
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.w600)),
+                            Text(userController.rubberList?.join(',') ?? 'None')
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Rating Point',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w600)),
+                    Text('${userController.ratingPoint}.LP',
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w500))
+                  ],
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    onPressed: null,
+                    child: const Text('Submit'))
+              ],
             )));
   }
 }
