5c4ee41e35cc268f0debaf01eadb06ecf598726e
leye195 <leye195@naver.com>
2023-08-12 20:22:49 +0900

    feat: add rank widget

--- lib/screens/rank_screen.dart
+++ lib/screens/rank_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tak_gg/states/UserController.dart';
+import 'package:tak_gg/widgets/rank.dart';
 
 class RankScreen extends StatefulWidget {
   const RankScreen({super.key});
@@ -131,58 +132,5 @@ class _RankScreenState extends State<RankScreen> {
             ],
           ),
         ));
-  }
-}
-
-class Rank extends StatelessWidget {
-  const Rank({
-    super.key,
-    required this.userController,
-    required this.rank,
-    required this.name,
-    required this.points,
-  });
-
-  final UserController userController;
-  final int rank;
-  final String name;
-  final num points;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Text(
-                '$rank',
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              CircleAvatar(
-                  radius: 25,
-                  backgroundImage: NetworkImage(userController.profileImage)),
-              const SizedBox(
-                width: 12,
-              ),
-              Text(
-                name,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-          Text(
-            '$points.LP',
-            style: const TextStyle(fontSize: 18),
-          )
-        ],
-      ),
-    );
   }
 }
---
+++ lib/widgets/rank.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tak_gg/states/UserController.dart';
+
+class Rank extends StatelessWidget {
+  const Rank({
+    super.key,
+    required this.userController,
+    required this.rank,
+    required this.name,
+    required this.points,
+  });
+
+  final UserController userController;
+  final int rank;
+  final String name;
+  final num points;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                '$rank',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(userController.profileImage)),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                name,
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          Text(
+            '$points.LP',
+            style: const TextStyle(fontSize: 18),
+          )
+        ],
+      ),
+    );
+  }
+}
