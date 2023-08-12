325060d399a547c35041d38e28de379441b62dbe
leye195 <leye195@naver.com>
2023-08-12 20:55:12 +0900

    style: set user rank on top_rank widget

--- lib/widgets/top_rank.dart
+++ lib/widgets/top_rank.dart
@@ -22,15 +22,50 @@ class TopRank extends StatelessWidget {
           radius: 30,
           backgroundImage: NetworkImage(
             profileImage
-          )),
-          const SizedBox(
-            height: 4,
-          ),
-          Container(
-            color: rank == 1 ? Colors.redAccent : rank == 2 ? Colors.amberAccent : Color.fromRGBO(152, 238, 204, 1),
-            width: 100,
-            height: rank == 1 ? 140 : rank == 2 ? 100 : 60,
           )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                )
+              ),
+              Text(
+                '${points}.LP',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                )
+              ),
+            ],
+          ) 
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Container(
+          color: rank == 1 ? Colors.redAccent : rank == 2 ? Colors.amberAccent : Color.fromRGBO(152, 238, 204, 1),
+          width: 100,
+          height: rank == 1 ? 140 : rank == 2 ? 100 : 60,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Text(
+                '$rank',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 28
+                )
+              )
+            ]
+          )
+        )
       ],
     );
   }
