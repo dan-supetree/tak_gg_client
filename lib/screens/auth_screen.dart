7844b6a4abb1ebce41453500197d2d66c489ef10
leye195 <leye195@naver.com>
2023-08-13 14:06:41 +0900

    style: adjust padding

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -21,7 +21,7 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(32.0),
+        padding: const EdgeInsets.only(top:32.0,left: 32,right:32,bottom: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -69,7 +69,6 @@ class _AuthScreenState extends State<AuthScreen> {
                             "playerId": player.playerId,
                             "profileImage": player.profileImage,
                             "displayName": player.displayName,
-                            "style": player.style,
                             "racket": player.racket,
                             "rubberList": player.rubberList,
                             "ratingPoint": player.ratingPoint,
