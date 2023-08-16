a5d74f1978d420f6ec494fc9c8230a65858a2f8d
leye195 <leye195@naver.com>
2023-08-16 23:23:42 +0900

    style: update shape of rank box to cube

--- lib/widgets/top_rank.dart
+++ lib/widgets/top_rank.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TopRank extends StatelessWidget {
-  const TopRank({
-    super.key,
-    required this.rank,
-    required this.name,
-    required this.profileImage,
-    required this.points
-  });
+  const TopRank(
+      {super.key,
+      required this.rank,
+      required this.name,
+      required this.profileImage,
+      required this.points});
 
   final String rank;
   final String name;
@@ -18,53 +17,62 @@ class TopRank extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundImage: NetworkImage(
-            profileImage
-          )
-        ),
+        CircleAvatar(radius: 30, backgroundImage: NetworkImage(profileImage)),
         Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Column(
-            children: [
-              Text(
-                name,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800,
-                )
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              children: [
+                Text(name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800,
+                    )),
+                Text('$points.LP',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ],
+            )),
+        Column(
+          children: [
+            Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.008)
+                ..rotateX(-1),
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: rank == '1'
+                    ? Colors.red
+                    : rank == '2'
+                        ? Colors.amber
+                        : const Color.fromRGBO(133, 230, 197, 1),
+                width: 100,
+                height: 24,
               ),
-              Text(
-                '$points.LP',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                )
-              ),
-            ],
-          ) 
-        ),
-        const SizedBox(
-          height: 4,
-        ),
-        Container(
-          color: rank == '1' ? Colors.redAccent : rank == '2' ? Colors.amberAccent : const Color.fromRGBO(152, 238, 204, 1),
-          width: 100,
-          height: rank == '1' ? 140 : rank == '2' ? 100 : 60,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              Text(
-                rank,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 28
-                )
-              )
-            ]
-          )
+            ),
+            Container(
+                color: rank == '1'
+                    ? Colors.redAccent
+                    : rank == '2'
+                        ? Colors.amberAccent
+                        : const Color.fromRGBO(152, 238, 204, 1),
+                width: 100,
+                height: rank == '1'
+                    ? 140
+                    : rank == '2'
+                        ? 100
+                        : 60,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(rank,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 28))
+                    ])),
+          ],
         )
       ],
     );
