b33ced5639df5406d7e2936db2b9ec3bfa623f06
dan-supetree <danyj@supertree.co>
2023-08-17 08:20:01 +0900

    feat: apply SingleChildScrollVIew

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -20,73 +20,76 @@ class _AuthScreenState extends State<AuthScreen> {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(top:32.0,left: 32,right:32,bottom: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage('assets/takgg_icon_rdbox.png'),
-              width: 200,
-              height: 200,
-            ),
-            const SizedBox(height: 32),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Token',
-                hintText: 'Enter your token',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide: BorderSide(width: 1, color: Colors.redAccent),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 32.0, left: 32, right: 32, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                image: AssetImage('assets/takgg_icon_rdbox.png'),
+                width: 200,
+                height: 200,
               ),
-              onChanged: (value) {
-                setState(() {
-                  token = value;
-                });
-              },
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50),
-                  shape: const StadiumBorder(),
-                  padding: const EdgeInsets.all(20),
+              const SizedBox(height: 32),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Token',
+                  hintText: 'Enter your token',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.redAccent),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
                 ),
-                onPressed: token == ''
-                    ? null
-                    : () async {
-                        try {
-                          final player =
-                              await ApiService.postAuthRequest(token);
+                onChanged: (value) {
+                  setState(() {
+                    token = value;
+                  });
+                },
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.all(20),
+                  ),
+                  onPressed: token == ''
+                      ? null
+                      : () async {
+                          try {
+                            final player =
+                                await ApiService.postAuthRequest(token);
 
-                          if (!mounted) return;
+                            if (!mounted) return;
 
-                          userController.updateUser({
-                            "playerId": player.playerId,
-                            "profileImage": player.profileImage,
-                            "displayName": player.displayName,
-                            "racket": player.racket,
-                            "rubberList": player.rubberList,
-                            "ratingPoint": player.ratingPoint,
-                          });
+                            userController.updateUser({
+                              "playerId": player.playerId,
+                              "profileImage": player.profileImage,
+                              "displayName": player.displayName,
+                              "racket": player.racket,
+                              "rubberList": player.rubberList,
+                              "ratingPoint": player.ratingPoint,
+                            });
 
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const HomeScreen()));
-                        } catch (e) {
-                          print('error:$e');
-                        }
-                      },
-                child: const Text(
-                  'Continue',
-                  style: TextStyle(fontSize: 20),
-                ))
-          ],
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomeScreen()));
+                          } catch (e) {
+                            print('error:$e');
+                          }
+                        },
+                  child: const Text(
+                    'Continue',
+                    style: TextStyle(fontSize: 20),
+                  ))
+            ],
+          ),
         ),
       ),
     );
