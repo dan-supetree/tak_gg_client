b13a8187d63da6f7eb6afcf4b677e6bee2ac0697
leye195 <leye195@naver.com>
2023-08-20 22:44:47 +0900

    feat: update PlayResultModel

--- lib/models/game_model.dart
+++ lib/models/game_model.dart
@@ -1,14 +1,14 @@
-class PlayResult {
+class PlayResultModel {
   final String playerId;
   final String displayName;
   final String profileImage;
   final int score;
 
-  PlayResult.fromJSON(Map<String, dynamic> json)
+  PlayResultModel.fromJSON(Map<String, dynamic> json)
       : playerId = json['playerId'],
         displayName = json['displayName'],
         profileImage = json['profileImage'],
-        score = json['profileImage'];
+        score = json['score'];
 }
 
 class GameModel {
@@ -16,14 +16,14 @@ class GameModel {
   final String status;
   final bool isWinner;
   final int ratingTransition;
-  final PlayResult winner;
-  final PlayResult loser;
+  final PlayResultModel winner;
+  final PlayResultModel loser;
 
   GameModel.fromJSON(Map<String, dynamic> json)
       : gameId = json['gameId'],
         status = json['status'],
         ratingTransition = json['ratingTransition'],
         isWinner = json['isWinner'],
-        winner = json['winner'],
-        loser = json['loser'];
+        winner = PlayResultModel.fromJSON(json['winner']),
+        loser = PlayResultModel.fromJSON(json['loser']);
 }
