4607bc3ac94eec7d248843c4501f6b9bda2a461e
leye195 <leye195@naver.com>
2023-08-20 22:54:49 +0900

    refactor: seperate result widget from history

---
+++ lib/widgets/result.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:tak_gg/models/game_model.dart';
+
+class Result extends StatelessWidget {
+  const Result({super.key, required this.winner, required this.loser});
+
+  final PlayResultModel winner;
+  final PlayResultModel loser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              width: 60,
+              child: Text(
+                winner.displayName,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            CircleAvatar(
+                radius: 14, backgroundImage: NetworkImage(winner.profileImage)),
+            const SizedBox(
+              width: 12,
+            ),
+            Text(
+              winner.score.toString().padLeft(2, '0'),
+              style: const TextStyle(
+                  fontSize: 18, fontWeight: FontWeight.w500, color: Colors.red),
+            ),
+          ],
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Text('End'),
+        const SizedBox(
+          width: 20,
+        ),
+        Row(
+          children: [
+            Text(
+              loser.score.toString().padLeft(2, '0'),
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            CircleAvatar(
+                radius: 14, backgroundImage: NetworkImage(loser.profileImage)),
+            const SizedBox(
+              width: 4,
+            ),
+            SizedBox(
+              width: 60,
+              child: Text(
+                loser.displayName,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
