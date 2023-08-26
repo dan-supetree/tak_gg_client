475582e5ed8bf18774643b8aa7b9cca4bda6f232
leye195 <leye195@naver.com>
2023-08-27 00:37:38 +0900

    feat: add item_model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,32 @@
+class RacketModel {
+  final String id;
+  final String name;
+
+  RacketModel.fromJSON(Map<String, dynamic> json)
+      : id = json['racket_id'],
+        name = json['name'];
+
+  @override
+  String toString() => name;
+}
+
+class RubberModel {
+  final String id;
+  final String name;
+
+  RubberModel.fromJSON(Map<String, dynamic> json)
+      : id = json['rubber_id'],
+        name = json['name'];
+
+  @override
+  String toString() => name;
+}
+
+class ItemModel {
+  final List<RacketModel> rackets;
+  final List<RubberModel> rubbers;
+
+  ItemModel.fromJSON(Map<String, dynamic> json)
+      : rackets = json['rackets'],
+        rubbers = json['rubbers'];
+}
