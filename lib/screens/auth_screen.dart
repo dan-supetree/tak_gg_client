4513d0b1f0eae357bf4896db64c85a8469d51a52
leye195 <leye195@naver.com>
2023-08-27 15:57:12 +0900

    feat: set bacgrounImage on authScreen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -18,81 +18,94 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     final UserController userController = Get.put(UserController());
 
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              const EdgeInsets.only(top: 32.0, left: 32, right: 32, bottom: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('assets/takgg_icon_rdbox.png'),
-                width: 200,
-                height: 200,
-              ),
-              const SizedBox(height: 32),
-              TextField(
-                decoration: const InputDecoration(
-                  labelText: 'Token',
-                  hintText: 'Enter your token',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(width: 1, color: Colors.redAccent),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/auth-bg.jpg'), // 배경 이미지
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 150.0, left: 32, right: 32, bottom: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/takgg_icon_rdbox.png'),
+                  width: 200,
+                  height: 200,
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    token = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50),
-                    shape: const StadiumBorder(),
-                    padding: const EdgeInsets.all(20),
+                const SizedBox(height: 32),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Token',
+                    hintText: 'Enter your token',
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(width: 1, color: Colors.redAccent),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
                   ),
-                  onPressed: token == ''
-                      ? null
-                      : () async {
-                          try {
-                            final player =
-                                await ApiService.postAuthRequest(token);
+                  onChanged: (value) {
+                    setState(() {
+                      token = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      shape: const StadiumBorder(),
+                      padding: const EdgeInsets.all(20),
+                      disabledBackgroundColor:
+                          const Color.fromRGBO(227, 230, 232, 1),
+                    ),
+                    onPressed: token == ''
+                        ? null
+                        : () async {
+                            try {
+                              final player =
+                                  await ApiService.postAuthRequest(token);
 
-                            if (!mounted) return;
+                              if (!mounted) return;
 
-                            userController.updateUser({
-                              "playerId": player.playerId,
-                              "profileImage": player.profileImage,
-                              "displayName": player.displayName,
-                              "racket": player.racket,
-                              "rubberList":
-                                  List<String>.from(player.rubberList ?? []),
-                              "ratingPoint": player.ratingPoint,
-                              "style": player.style
-                            });
+                              userController.updateUser({
+                                "playerId": player.playerId,
+                                "profileImage": player.profileImage,
+                                "displayName": player.displayName,
+                                "racket": player.racket,
+                                "rubberList":
+                                    List<String>.from(player.rubberList ?? []),
+                                "ratingPoint": player.ratingPoint,
+                                "style": player.style
+                              });
 
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    settings:
-                                        const RouteSettings(name: '/home'),
-                                    builder: (context) => const HomeScreen()));
-                          } catch (e) {
-                            print('error:$e');
-                          }
-                        },
-                  child: const Text(
-                    'Continue',
-                    style: TextStyle(fontSize: 20),
-                  ))
-            ],
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      settings:
+                                          const RouteSettings(name: '/home'),
+                                      builder: (context) =>
+                                          const HomeScreen()));
+                            } catch (e) {
+                              print('error:$e');
+                            }
+                          },
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(fontSize: 20),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
