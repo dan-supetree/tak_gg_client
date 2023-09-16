24a93d67fe14dd03620fae2e7437219baefc9764
leye195 <leye195@naver.com>
2023-09-17 02:20:43 +0900

    style: update result submit screen layout

--- lib/screens/result_submit_screen.dart
+++ lib/screens/result_submit_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_material_pickers/flutter_material_pickers.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:tak_gg/models/player_model.dart';
 import 'package:tak_gg/services/api_service.dart';
 import 'package:tak_gg/states/user_controller.dart';
+import 'package:tak_gg/widgets/player_dropdown.dart';
 
 class ResultSubmitScreen extends StatefulWidget {
   const ResultSubmitScreen({super.key});
@@ -13,10 +14,12 @@ class ResultSubmitScreen extends StatefulWidget {
 }
 
 class _ResultSubmitScreenState extends State<ResultSubmitScreen> {
+  int step = 0;
   int player1Score = 0;
   int player2Score = 0;
-  PlayerModel? selected;
+  String? selected;
   List<PlayerModel> users = [];
+
   final UserController userController = Get.put(UserController());
 
   bool isDisabled() {
@@ -25,23 +28,6 @@ class _ResultSubmitScreenState extends State<ResultSubmitScreen> {
     }
 
     return false;
-  }
-
-  void selectPlayer() {
-    showMaterialScrollPicker(
-        context: context,
-        items: users,
-        title: 'Select player',
-        selectedItem: selected,
-        headerColor: Colors.blueGrey[900],
-        buttonTextColor: Colors.blueGrey[900],
-        onChanged: (value) {
-          final user = users
-              .firstWhere((item) => item.displayName == value?.displayName);
-          setState(() {
-            selected = user;
-          });
-        });
   }
 
   void fetchUserList() async {
@@ -61,7 +47,7 @@ class _ResultSubmitScreenState extends State<ResultSubmitScreen> {
         'score': player1Score,
       },
       {
-        'playerId': selected?.playerId,
+        'playerId': selected,
         'score': player2Score,
       }
     ];
@@ -94,116 +80,309 @@ class _ResultSubmitScreenState extends State<ResultSubmitScreen> {
     fetchUserList();
   }
 
+  Widget controlsBuilder(context, details) {
+    return step <= 1
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: step == 0 ? null : details.onStepCancel,
+                    child: const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text('Back',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )),
+                    )),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: details.onStepContinue,
+                    child: const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text('Next',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )),
+                    )),
+              ),
+            ],
+          )
+        : Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: details.onStepCancel,
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text('Back',
+                        style: TextStyle(
+                          fontSize: 18,
+                        )),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                    onPressed: player1Score < 11 ||
+                            player2Score < 11 ||
+                            selected == null
+                        ? null
+                        : submitResult,
+                    child: const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text('Submit',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )),
+                    )),
+              ),
+            ],
+          );
+  }
+
+  onTapStep(value) {
+    setState(() {
+      step = value;
+    });
+  }
+
+  onContinueStep() {
+    if (step < 2) {
+      setState(() {
+        step = step + 1;
+      });
+    }
+  }
+
+  onCancelStep() {
+    if (step > 0) {
+      setState(() {
+        step = step - 1;
+      });
+    }
+  }
+
+  onChangeMyScore(value) {
+    setState(() {
+      player1Score = int.parse(value);
+    });
+  }
+
+  onChangeOpponentScore(value) {
+    setState(() {
+      player2Score = int.parse(value);
+    });
+  }
+
+  onSelectOpponent(value) {
+    setState(() {
+      selected = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Submit Game Result',
-            style: TextStyle(color: Colors.white),
+    double height = MediaQuery.of(context).size.height;
+
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Submit Game Result',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+            iconTheme: const IconThemeData(color: Colors.white),
           ),
-          backgroundColor: Colors.blueGrey[900],
-          iconTheme: const IconThemeData(color: Colors.white),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.only(
-                top: 32.0, bottom: 32, left: 16, right: 16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            showMaterialNumberPicker(
-                              context: context,
-                              minNumber: 0,
-                              maxNumber: 21,
-                              title: 'Pick the Game Score',
-                              headerColor: Colors.blueGrey[900],
-                              selectedNumber: player1Score,
-                              onChanged: (value) {
-                                setState(() {
-                                  player1Score = value;
-                                });
-                              },
-                              cancellable: false,
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Text(player1Score.toString().padLeft(2, '0'),
-                                style: const TextStyle(
-                                    fontSize: 100,
-                                    fontWeight: FontWeight.w700)),
+          body: Stepper(
+            currentStep: step,
+            type: StepperType.horizontal,
+            onStepTapped: onTapStep,
+            onStepContinue: onContinueStep,
+            onStepCancel: onCancelStep,
+            controlsBuilder: controlsBuilder,
+            steps: [
+              Step(
+                  isActive: step >= 0,
+                  state: step >= 0 ? StepState.complete : StepState.disabled,
+                  title: const Text(''),
+                  content: SizedBox(
+                    height: height * 0.5,
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 32.0, bottom: 32, left: 0, right: 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'What is your score?',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(height: 20),
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: const InputDecoration(
+                                labelText: 'Score',
+                                hintText: 'Enter your Score',
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.redAccent),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0)),
+                                ),
+                              ),
+                              onChanged: onChangeMyScore,
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            const Text('Please check your score before submit',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                )),
+                          ],
+                        )),
+                  )),
+              Step(
+                  isActive: step >= 1,
+                  state: step >= 1 ? StepState.complete : StepState.disabled,
+                  title: const Text(''),
+                  content: SizedBox(
+                    height: height * 0.5,
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 32.0, bottom: 32, left: 0, right: 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'What is opponent score?',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(height: 20),
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              inputFormatters: <TextInputFormatter>[
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
+                              decoration: const InputDecoration(
+                                labelText: 'Score',
+                                hintText: 'Enter Score',
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0)),
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.redAccent),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10.0)),
+                                ),
+                              ),
+                              onChanged: onChangeOpponentScore,
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            const Text('Please check your score before submit',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                )),
+                          ],
+                        )),
+                  )),
+              Step(
+                  isActive: step >= 2,
+                  state: step >= 2 ? StepState.complete : StepState.disabled,
+                  title: const Text(''),
+                  content: SizedBox(
+                    height: height * 0.5,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 32.0, bottom: 32, left: 0, right: 0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          PlayerDropdown(
+                              selected: selected,
+                              dropdownHandler: onSelectOpponent),
+                          const SizedBox(
+                            height: 20,
                           ),
-                        ),
-                        const SizedBox(height: 12),
-                        ElevatedButton(
-                          onPressed: null,
-                          child: Text(userController.displayName,
-                              style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.black)),
-                        )
-                      ],
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.black12,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text('You',
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.w600)),
+                                  SizedBox(width: 18),
+                                  Text(' VS '),
+                                  SizedBox(width: 18),
+                                  Text('???',
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.w600))
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                player1Score.toString().padLeft(2, '0'),
+                                style: const TextStyle(
+                                    fontSize: 45, fontWeight: FontWeight.w600),
+                              ),
+                              const Text(' : ', style: TextStyle(fontSize: 40)),
+                              Text(player2Score.toString().padLeft(2, '0'),
+                                  style: const TextStyle(
+                                      fontSize: 45,
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    const SizedBox(width: 12),
-                    Column(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            showMaterialNumberPicker(
-                              context: context,
-                              minNumber: 0,
-                              maxNumber: 21,
-                              headerColor: Colors.blueGrey[900],
-                              title: 'Pick the Game Score',
-                              selectedNumber: player2Score,
-                              onChanged: (value) {
-                                setState(() {
-                                  player2Score = value;
-                                });
-                              },
-                              cancellable: false,
-                            );
-                          },
-                          child: Text(player2Score.toString().padLeft(2, '0'),
-                              style: const TextStyle(
-                                  fontSize: 100, fontWeight: FontWeight.w700)),
-                        ),
-                        const SizedBox(height: 12),
-                        ElevatedButton(
-                          onPressed: selectPlayer,
-                          child: Text(
-                              selected != null
-                                  ? selected?.displayName ?? ''
-                                  : 'Player',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.white,
-                              )),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    onPressed: isDisabled() ? null : submitResult,
-                    child: const Text('Submit',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ))),
-              ],
-            )));
+                  ))
+            ],
+          )),
+    );
   }
 }
